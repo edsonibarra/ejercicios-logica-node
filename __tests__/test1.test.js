a776b5a783d3fe109a14c14a702e865b36fc90cd
Oscar Islas <oislasreyes@gmail.com>
2021-02-08 17:14:23 -0600

    Primer ejercicio logico

---
+++ __tests__/test1.test.js
@@ -0,0 +1,20 @@
+const {divisibleByLeft} = require("../ejercicio1");
+
+describe("Divisible por el digito de la izquieda", () => {
+    test("Probando 73312", () => {
+        expect(divisibleByLeft(73312)).toEqual([false, false, true, false, true]);
+    });
+
+    test("Probando 1", () => {
+        expect(divisibleByLeft(1)).toEqual([false]);
+    });
+
+    test("Probando 635", () => {
+        expect(divisibleByLeft(635)).toEqual([false, false, false]);
+    });
+
+    test("Probando 748", () => {
+        expect(divisibleByLeft(748)).toEqual([false, false, true]);
+    });
+
+});
---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+// ¿Divisible por el digito de la izquierda?
+// Crea una función que tome un numero n y revisa si cada digito es divisible por el digito que tiene a su izquierda
+// Regresa un arreglo booleano dependiendo si la condición se cumple.
+
+// Por ejemplo: 
+// Input = 73312
+// Output = [false, false, true, false, true]
+// a.	No hay digito a la izquierda de 7 -> false
+// b.	3/7 = false
+// c.	3/3 = true
+// d.	1/3 = false
+// e.	2/1 = true
+
+
+// Nota: El arreglo siempre debe de empezar con un valor en falso.
+function divisibleByLeft(n) {
+    //Regresar el arreglo de la respuesta
+	return 0;
+}
+
+module.exports = {
+    divisibleByLeft
+}
